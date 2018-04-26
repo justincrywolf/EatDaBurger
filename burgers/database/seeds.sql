795152d2f06858e4dbb6d9918bbdf8712bf55609
justincrywolf <justincrywolf@gmail.com>
2018-04-26 17:55:10 -0400

    whew

---
+++ burgers/database/seeds.sql
@@ -0,0 +1,5 @@
+USE burgers_db;
+
+INSERT INTO burgers(burger_name, devoured) VALUES( "veggie burger", FALSE );
+INSERT INTO burgers(burger_name, devoured) VALUES( "black bean burger", FALSE );
+INSERT INTO burgers(burger_name, devoured) VALUES( "cheeseburger", FALSE );
